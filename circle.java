856077c34f1a9c96b095e8132b5e64a96171315a
Dilip Kumar <dilipkumar3914@gmail.com>
2022-06-04 14:19:56 +0530

    Circle.java file is added

---
+++ circle.java
@@ -0,0 +1,34 @@
+class Circle
+{
+	int centerX;
+	int centerY;
+	int radius;
+	
+	void init()
+	{
+		centerX=0;
+		centerY=0;
+		radius=1;
+	}
+	
+	void init(int x,int y)
+	{
+		centerX=x;
+		centerY=y;
+		radius=1;
+	}
+
+
+	void init(int x,int y,int z)
+	{
+		centerX=x;
+		centerY=y;
+		radius=z;
+	}
+	
+	void print()
+	{
+		System.out.println("Center :(" + centerX + "," + centerY + ") Radius : "+radius);
+	} 
+	
+}
